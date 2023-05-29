9ab4ca2506f0507e9d63ef1a9ac45d2dbaa34ebf
RubenRaMar <rubenfr1@hotmail.com>
2023-05-29 23:51:20 +0200

    Test NavegationMenu component and their use cases

---
+++ src/components/NavegationMenu/NavegationMenu.test.tsx
@@ -0,0 +1,37 @@
+import { screen } from "@testing-library/react";
+import NavegationMenu from "./NavegationMenu";
+import { renderWithProviders } from "../../utils/testUtils";
+
+describe("Given a NavegationMenu component", () => {
+  describe("When it rendering", () => {
+    test("Then it should show three icons", () => {
+      const expectedAlternativesTexts = [
+        "All figures icon",
+        "Filter pending figures icon",
+        "Add figures icon",
+      ];
+
+      renderWithProviders(<NavegationMenu />);
+
+      expectedAlternativesTexts.map((expectedAlternativeText) => {
+        const image = screen.getByRole("img", {
+          name: expectedAlternativeText,
+        });
+
+        expect(image).toBeInTheDocument();
+      });
+    });
+
+    test("Then it should show the 'All', 'Pending', and 'Add' texts", () => {
+      const expectedTexts = ["All", "Pending", "Add"];
+
+      renderWithProviders(<NavegationMenu />);
+
+      expectedTexts.map((expectedText) => {
+        const text = screen.getByText(expectedText);
+
+        expect(text).toBeInTheDocument();
+      });
+    });
+  });
+});
