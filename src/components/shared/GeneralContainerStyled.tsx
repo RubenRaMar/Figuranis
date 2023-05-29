1425bdf62a736a5a0878de88f072e20ece994ee8
RubenRaMar <rubenfr1@hotmail.com>
2023-05-29 20:26:57 +0200

    Create the shared component GeneralContainerStyled

---
+++ src/components/shared/GeneralContainerStyled.tsx
@@ -0,0 +1,9 @@
+import styled from "styled-components";
+import toRem from "../../styles/functions/toRem";
+
+const GeneralContainerStyled = styled.div`
+  margin: 0 auto;
+  padding: ${toRem(22)};
+`;
+
+export default GeneralContainerStyled;
