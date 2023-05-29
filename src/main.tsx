b3b448f6ec9938a18da1d912e51efd84d9ec5c5c
RubenRaMar <rubenfr1@hotmail.com>
2023-05-29 19:27:45 +0200

    Create theme containing style variables

--- src/main.tsx
+++ src/main.tsx
@@ -1,14 +1,19 @@
-import React from "react";
+import { ThemeProvider } from "styled-components";
 import ReactDOM from "react-dom/client";
-import App from "./components/App/App";
-import "./index.css";
 import { Provider } from "react-redux";
 import { store } from "./store";
+import React from "react";
+import "@fontsource/";
+import App from "./components/App/App";
+import "./index.css";
+import theme from "./styles/theme/theme.js";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
-    <Provider store={store}>
-      <App />
-    </Provider>
+    <ThemeProvider theme={theme}>
+      <Provider store={store}>
+        <App />
+      </Provider>
+    </ThemeProvider>
   </React.StrictMode>
 );
