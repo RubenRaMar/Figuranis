53da4ed5b208a8915612643832da4f3a0b3643dd
RubenRaMar <rubenfr1@hotmail.com>
2023-05-30 16:56:52 +0200

    Create LoginPage component

--- src/components/Layout/Layout.tsx
+++ src/components/Layout/Layout.tsx
@@ -1,15 +1,12 @@
 import React from "react";
 import { Outlet } from "react-router-dom";
-import GeneralContainerStyled from "../shared/GeneralContainerStyled.js";
 import MainHeader from "../MainHeader/MainHeader.js";
 
 const Layout = (): React.ReactElement => {
   return (
     <>
       <MainHeader />
-      <GeneralContainerStyled>
-        <Outlet />
-      </GeneralContainerStyled>
+      <Outlet />
     </>
   );
 };
