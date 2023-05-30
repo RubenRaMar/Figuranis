ece4ac71581ddc02bc7ad155f0d910da14a6b8df
RubenRaMar <rubenfr1@hotmail.com>
2023-05-30 14:00:26 +0200

    Create MainHeader component

--- src/routers/appRouter.tsx
+++ src/routers/appRouter.tsx
@@ -1,13 +1,10 @@
-import { Navigate, RouteObject, createBrowserRouter } from "react-router-dom";
+import { RouteObject, createBrowserRouter } from "react-router-dom";
 import App from "../components/App/App";
 
 const routes: RouteObject[] = [
   {
     path: "/",
     element: <App />,
-    children: [
-      { index: true, element: <Navigate to={"/user/login"} replace /> },
-    ],
   },
 ];
 
