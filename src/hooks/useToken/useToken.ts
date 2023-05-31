3fd846579fbc4d76b69736a38b20fb20c0d20d87
RubenRaMar <rubenfr1@hotmail.com>
2023-05-31 20:41:37 +0200

    Create getLoginToken custom hook to decode token and obtain user data

---
+++ src/hooks/useToken/useToken.ts
@@ -0,0 +1,20 @@
+import jwt_decode from "jwt-decode";
+import { useCallback } from "react";
+import { UserTokenStructure } from "../types";
+
+const useToken = () => {
+  const getLoginToken = useCallback((token: string): UserTokenStructure => {
+    const decodeToken: { sub: string; name: string } = jwt_decode(token);
+    const userData = {
+      id: decodeToken.sub,
+      username: decodeToken.name,
+      token,
+    };
+
+    return userData;
+  }, []);
+
+  return { getLoginToken };
+};
+
+export default useToken;
