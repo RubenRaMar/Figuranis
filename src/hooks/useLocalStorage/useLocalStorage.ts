3c5433670d1cc4da0f2e7ff2d252af3ef3a1e1d8
RubenRaMar <rubenfr1@hotmail.com>
2023-05-31 22:20:30 +0200

    Create the function getToken to request the token from local storage

--- src/hooks/useLocalStorage/useLocalStorage.ts
+++ src/hooks/useLocalStorage/useLocalStorage.ts
@@ -2,7 +2,9 @@ const useLocalStorage = () => {
   const setToken = (key: string, value: string) =>
     localStorage.setItem(key, value);
 
-  return { setToken };
+  const getToken = (key: string) => localStorage.getItem(key);
+
+  return { setToken, getToken };
 };
 
 export default useLocalStorage;
