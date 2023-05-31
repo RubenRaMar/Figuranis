f0c6e8c18865eb83567022bfa0f134959bd5e6e9
RubenRaMar <rubenfr1@hotmail.com>
2023-05-31 22:31:10 +0200

    Test setToken and getToken and their use cases

---
+++ src/hooks/useLocalStorage/useLocalStorage.test.ts
@@ -0,0 +1,27 @@
+import { renderHook } from "@testing-library/react";
+import useLocalStorage from "./useLocalStorage";
+
+describe("Given the setToken and getToken functions", () => {
+  describe("When invoked to save a token", () => {
+    test("Then it should be able to access the token at the localStorage", () => {
+      const key = "token";
+      const value = "tokenStorage";
+
+      const {
+        result: {
+          current: { getToken },
+        },
+      } = renderHook(() => useLocalStorage());
+
+      const {
+        result: {
+          current: { setToken },
+        },
+      } = renderHook(() => useLocalStorage());
+
+      setToken(key, value);
+
+      expect(getToken(key)).toStrictEqual(value);
+    });
+  });
+});
