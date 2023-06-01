f3726419945acbdbaa96226680429572fb411b7c
RubenRaMar <rubenfr1@hotmail.com>
2023-06-01 21:35:51 +0200

    Implement styles to FiguresPage

---
+++ src/pages/FiguresPage/FiguresPageStyled.ts
@@ -0,0 +1,9 @@
+import styled from "styled-components";
+
+const FiguresPageStyled = styled.section`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`;
+
+export default FiguresPageStyled;
