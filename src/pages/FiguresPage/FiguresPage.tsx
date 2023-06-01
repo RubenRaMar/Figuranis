5f7bb0ed3d7b16f0b38bde7ca5a48941306dd81a
RubenRaMar <rubenfr1@hotmail.com>
2023-06-01 21:34:35 +0200

    Create FiguresPage component

---
+++ src/pages/FiguresPage/FiguresPage.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import FiguresPageStyled from "./FiguresPageStyled";
+
+const FiguresPage = (): React.ReactElement => {
+  return (
+    <FiguresPageStyled>
+      <h1>
+        Your favorite <br /> figures
+      </h1>
+    </FiguresPageStyled>
+  );
+};
+
+export default FiguresPage;
