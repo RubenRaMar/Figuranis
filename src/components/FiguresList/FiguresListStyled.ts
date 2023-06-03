6d93629daf25a8090846a0a2be673359fecc0cf1
RubenRaMar <rubenfr1@hotmail.com>
2023-06-03 22:53:00 +0200

    Implement getFiguresList custom hook in FiguresList component

--- src/components/FiguresList/FiguresListStyled.ts
+++ src/components/FiguresList/FiguresListStyled.ts
@@ -1,9 +1,23 @@
 import styled from "styled-components";
+import toRem from "../../styles/functions/toRem";
 
 const FiguresListStyled = styled.ul`
   display: flex;
   flex-direction: column;
   gap: 50px;
+
+  .figures-not-found {
+    display: flex;
+    flex-direction: column;
+    gap: 38px;
+    align-items: center;
+    margin-top: 30px;
+
+    span {
+      text-align: center;
+      font-size: ${toRem(22)};
+    }
+  }
 `;
 
 export default FiguresListStyled;
--- src/pages/FiguresPage/FiguresPage.tsx
+++ src/pages/FiguresPage/FiguresPage.tsx
@@ -2,13 +2,23 @@ import React from "react";
 import FiguresPageStyled from "./FiguresPageStyled";
 import { useAppDispatch } from "../../store";
 import { loadFiguresActionCreator } from "../../store/figures/figureSlice";
-import { figuresMock } from "../../mocks/figures/figures";
 import FiguresList from "../../components/FiguresList/FiguresList";
+import useFigures from "../../hooks/useFigure/useFigure";
+import { useEffect } from "react";
 
 const FiguresPage = (): React.ReactElement => {
   const dispatch = useAppDispatch();
+  const { getFigureList } = useFigures();
 
-  dispatch(loadFiguresActionCreator(figuresMock));
+  useEffect(() => {
+    (async () => {
+      const figureList = await getFigureList();
+
+      if (figureList) {
+        dispatch(loadFiguresActionCreator(figureList));
+      }
+    })();
+  }, [dispatch, getFigureList]);
 
   return (
     <FiguresPageStyled>
