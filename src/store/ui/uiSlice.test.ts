9c7acad8ab0e3056529ad3c9021767f5a47cd44e
RubenRaMar <rubenfr1@hotmail.com>
2023-06-04 20:46:02 +0200

    Test hideLoading mini reducer and their use cases

--- src/store/ui/uiSlice.test.ts
+++ src/store/ui/uiSlice.test.ts
@@ -1,11 +1,29 @@
-import { initialUiState, showLoadingActionCreator, uiReducer } from "./uiSlice";
+import { UiStructure } from "../../types";
+import {
+  hideLoadingActionCreator,
+  initialUiState,
+  showLoadingActionCreator,
+  uiReducer,
+} from "./uiSlice";
+
+const isLoadingTrue: UiStructure = { isLoading: true };
 
 describe("Given a showLoading mini reducer", () => {
   describe("When it invoked", () => {
     test("Then it should change the isLoading state to true", () => {
       const isLoading = uiReducer(initialUiState, showLoadingActionCreator());
 
-      expect(isLoading).toStrictEqual({ isLoading: true });
+      expect(isLoading).toStrictEqual(isLoadingTrue);
+    });
+  });
+});
+
+describe("Given a hideLoading mini reducer", () => {
+  describe("When it invoked", () => {
+    test("then it should change the isLoading state to false", () => {
+      const isLoading = uiReducer(isLoadingTrue, hideLoadingActionCreator());
+
+      expect(isLoading).toStrictEqual(initialUiState);
     });
   });
 });
