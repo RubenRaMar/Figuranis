5aa88c301d5c54b49306476367ac7928cd4d86fa
RubenRaMar <rubenfr1@hotmail.com>
2023-06-05 17:29:14 +0200

    Test showModal mini reducer and their use cases

--- src/store/ui/uiSlice.test.ts
+++ src/store/ui/uiSlice.test.ts
@@ -2,6 +2,7 @@ import {
   hideLoadingActionCreator,
   initialUiState,
   showLoadingActionCreator,
+  showModalActionCreator,
   uiReducer,
 } from "./uiSlice";
 
@@ -19,10 +20,32 @@ describe("Given a showLoading mini reducer", () => {
 
 describe("Given a hideLoading mini reducer", () => {
   describe("When it invoked", () => {
-    test("then it should change the isLoading state to false", () => {
+    test("Then it should change the isLoading property to false", () => {
       const isLoading = uiReducer(isLoadingTrue, hideLoadingActionCreator());
 
       expect(isLoading).toStrictEqual(initialUiState);
+    });
+  });
+});
+
+describe("Given a showModal mini reducer", () => {
+  describe("When it invoked to show an error message 'Wrong credentials'", () => {
+    test("Then it should chenge the idModal and error poperty to true and the message to 'Wrong credentials'", () => {
+      const modalErrorMock = {
+        isModal: true,
+        error: true,
+        message: "'Wrong credential",
+      };
+
+      const modalError = uiReducer(
+        initialUiState,
+        showModalActionCreator(modalErrorMock)
+      );
+
+      expect(modalError).toStrictEqual({
+        ...initialUiState,
+        modal: modalErrorMock,
+      });
     });
   });
 });
