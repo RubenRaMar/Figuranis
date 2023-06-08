2394c05fbe78561afb61b4025818b5e1f3539194
RubenRaMar <rubenfr1@hotmail.com>
2023-06-08 20:42:59 +0200

    Create form status and link all fields to the status

--- src/components/GenericForm/GenericForm.tsx
+++ src/components/GenericForm/GenericForm.tsx
@@ -1,18 +1,35 @@
-import React from "react";
+import React, { useState } from "react";
 import GenericFormStyled from "./GenericFormStyled";
 import { FiguresDataStructures } from "../../types";
 import GenericButton from "../GenericButton/GenericButton";
 
 interface GenericFormProps {
+  actionOnClick: (figure: Partial<FiguresDataStructures>) => void;
   textButton: string;
   buttonClassName: string;
-  figure: FiguresDataStructures;
 }
 
 const GenericForm = ({
+  actionOnClick,
   textButton,
   buttonClassName,
-  figure: {
+}: GenericFormProps): React.ReactElement => {
+  const initialFigureState: Partial<FiguresDataStructures> = {
+    title: "",
+    character: "",
+    franchise: "",
+    image: "",
+    manufacturer: "",
+    material: "",
+    price: 0,
+    purchased: false,
+    size: 0,
+    weight: 0,
+  };
+
+  const [figureData, setFigureData] = useState(initialFigureState);
+
+  const {
     title,
     character,
     franchise,
@@ -20,86 +37,154 @@ const GenericForm = ({
     manufacturer,
     material,
     price,
+    purchased,
     size,
     weight,
-  },
-}: GenericFormProps): React.ReactElement => {
+  } = figureData;
+
+  const isDisabled =
+    title !== "" &&
+    character !== "" &&
+    franchise !== "" &&
+    image !== "" &&
+    manufacturer !== "" &&
+    material !== "" &&
+    price !== 0 &&
+    size !== 0 &&
+    weight !== 0;
+
+  const onChangeFigureData = (event: React.ChangeEvent<HTMLInputElement>) => {
+    event.target.id === "purchased"
+      ? setFigureData({
+          ...figureData,
+          purchased: !purchased,
+        })
+      : setFigureData({
+          ...figureData,
+          [event.target.id]: event.target.value,
+        });
+  };
+
+  const handleLoginSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    event?.preventDefault();
+    actionOnClick(figureData);
+  };
+
   return (
-    <GenericFormStyled>
+    <GenericFormStyled onSubmit={handleLoginSubmit}>
       <h2 className="heading">
         Insert all the fields to be able
         <br /> to add a figure
       </h2>
-
       <div className="controler">
         <label htmlFor="title">title</label>
-        <input type="text" id="title" autoComplete="off" value={title} />
+        <input
+          type="text"
+          id="title"
+          autoComplete="off"
+          onChange={onChangeFigureData}
+          value={title}
+        />
       </div>
-
       <div className="controler">
         <label htmlFor="character">character</label>
         <input
           type="text"
           id="character"
           autoComplete="off"
+          onChange={onChangeFigureData}
           value={character}
         />
       </div>
-
       <div className="controler">
         <label htmlFor="franchise">franchise</label>
         <input
           type="text"
           id="franchise"
           autoComplete="off"
+          onChange={onChangeFigureData}
           value={franchise}
         />
       </div>
-
       <div className="controler">
         <label htmlFor="manufacturer">manufacturer</label>
         <input
           type="text"
           id="manufacturer"
           autoComplete="off"
+          onChange={onChangeFigureData}
           value={manufacturer}
         />
       </div>
-
       <div className="controler">
         <label htmlFor="material">material</label>
-        <input type="text" id="material" autoComplete="off" value={material} />
+        <input
+          type="text"
+          id="material"
+          autoComplete="off"
+          onChange={onChangeFigureData}
+          value={material}
+        />
       </div>
-
       <div className="controler">
         <label htmlFor="image">image(Url)</label>
-        <input type="text" id="image" autoComplete="off" value={image} />
+        <input
+          type="url"
+          id="image"
+          autoComplete="off"
+          onChange={onChangeFigureData}
+          value={image}
+        />
       </div>
-
       <div className="controler number">
         <label htmlFor="size">size</label>
-        <input type="number" id="size" autoComplete="off" value={size} />
+        <input
+          type="number"
+          id="size"
+          autoComplete="off"
+          onChange={onChangeFigureData}
+          value={size ? size : ""}
+        />
         <span className="symbol">Cm</span>
       </div>
-
       <div className="controler number">
         <label htmlFor="weight">weight</label>
-        <input type="number" id="weight" autoComplete="off" value={weight} />
+        <input
+          type="number"
+          id="weight"
+          autoComplete="off"
+          onChange={onChangeFigureData}
+          value={weight ? weight : ""}
+        />
         <span className="symbol">Kg</span>
       </div>
-
       <div className="controler number">
         <label htmlFor="price">price</label>
-        <input type="number" id="price" autoComplete="off" value={price} />
+        <input
+          type="number"
+          id="price"
+          autoComplete="off"
+          onChange={onChangeFigureData}
+          value={price ? price : ""}
+        />
         <span className="symbol">€</span>
       </div>
-
       <div className="purchased">
         <label htmlFor="purchased">purchased</label>
-        <input className="check" type="checkbox" id="purchased" />
+        <input
+          className="check"
+          type="checkbox"
+          id="purchased"
+          onChange={onChangeFigureData}
+          checked={purchased}
+        />
       </div>
 
-      <GenericButton className={buttonClassName} text={textButton} />
+      <GenericButton
+        isDisabled={!isDisabled}
+        className={buttonClassName}
+        text={textButton}
+      />
     </GenericFormStyled>
   );
 };
--- src/pages/AddFigurePage/AddFigurePage.tsx
+++ src/pages/AddFigurePage/AddFigurePage.tsx
@@ -1,6 +1,5 @@
 import AddFigurePageStyled from "./AddFigurePageStyled";
 import GenericForm from "../../components/GenericForm/GenericForm";
-import { figuresMock } from "../../mocks/figures/figures";
 
 const AddFiguresPage = (): React.ReactElement => {
   return (
@@ -8,11 +7,7 @@ const AddFiguresPage = (): React.ReactElement => {
       <h1>
         Add your favorite <br /> figure
       </h1>
-      <GenericForm
-        buttonClassName="add"
-        figure={figuresMock[0]}
-        textButton="Add figure"
-      />
+      <GenericForm buttonClassName="add" textButton="Add figure" />
     </AddFigurePageStyled>
   );
 };
