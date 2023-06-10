8507e972e4374548c85f566f41f92276b3355c4a
RubenRaMar <rubenfr1@hotmail.com>
2023-06-11 01:34:00 +0200

    Test implementation of the Pagination component

--- src/components/Pagination/Pagination.test.tsx
+++ src/components/Pagination/Pagination.test.tsx
@@ -1,27 +1,52 @@
+import { vi } from "vitest";
 import { renderWithProviders } from "../../utils/testUtils";
 import Pagination from "./Pagination";
 import { screen } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
 
 describe("Given a Pagination component", () => {
   describe("When it is rendered", () => {
-    test("Then it should show a 'Previous' button", () => {
+    const actionOnNextButton = vi.fn();
+    const actionOnPreviousButton = vi.fn();
+
+    test("Then it should show a 'Previous' button", async () => {
       const expectedButtonText = "previous";
 
-      renderWithProviders(<Pagination />);
+      renderWithProviders(
+        <Pagination
+          nextPage={actionOnNextButton}
+          previousPage={actionOnPreviousButton}
+          skip={1}
+          totalFigures={10}
+        />
+      );
 
       const button = screen.getByRole("button", { name: expectedButtonText });
 
+      await userEvent.click(button);
+
       expect(button).toBeInTheDocument();
+      expect(actionOnPreviousButton).toHaveBeenCalled();
     });
 
-    test("Then it should show a 'next' button", () => {
+    test("Then it should show a 'next' button", async () => {
       const expectedButtonText = "next";
 
-      renderWithProviders(<Pagination />);
+      renderWithProviders(
+        <Pagination
+          nextPage={actionOnNextButton}
+          previousPage={actionOnPreviousButton}
+          skip={0}
+          totalFigures={1}
+        />
+      );
 
       const button = screen.getByRole("button", { name: expectedButtonText });
 
+      await userEvent.click(button);
+
       expect(button).toBeInTheDocument();
+      expect(actionOnNextButton).toHaveBeenCalled();
     });
   });
 });
--- src/pages/FiguresPage/FiguresPage.test.tsx
+++ src/pages/FiguresPage/FiguresPage.test.tsx
@@ -1,8 +1,14 @@
 import { screen } from "@testing-library/react";
 import FiguresPage from "./FiguresPage";
 import { renderWithProviders } from "../../utils/testUtils";
+import Pagination from "../../components/Pagination/Pagination";
+import { vi } from "vitest";
+import userEvent from "@testing-library/user-event";
 
 describe("Given a FiguresPage component", () => {
+  const nextPage = vi.fn();
+  const previousPage = vi.fn();
+
   describe("When it rendered", () => {
     test("Then it should show a heading with a 'Your favorite figures'", () => {
       const expectedAltText = "Your favorite figures";
@@ -12,6 +18,47 @@ describe("Given a FiguresPage component", () => {
       const heading = screen.getByRole("heading", { name: expectedAltText });
 
       expect(heading).toBeInTheDocument();
+    });
+  });
+
+  describe("When it is rendered and the user clicks on the 'next' button", () => {
+    test("Then it should call the function nextPage", async () => {
+      renderWithProviders(
+        <Pagination
+          nextPage={nextPage}
+          previousPage={previousPage}
+          skip={1}
+          totalFigures={10}
+        />
+      );
+
+      const expectedButtonText = "next";
+
+      const button = screen.getByRole("button", { name: expectedButtonText });
+
+      await userEvent.click(button);
+
+      expect(nextPage).toHaveBeenCalled();
+    });
+  });
+  describe("When it is rendered and the user clicks on the 'previous' button", () => {
+    test("Then it should call the function previousPage", async () => {
+      renderWithProviders(
+        <Pagination
+          nextPage={nextPage}
+          previousPage={previousPage}
+          skip={1}
+          totalFigures={10}
+        />
+      );
+
+      const expectedButtonText = "previous";
+
+      const button = screen.getByRole("button", { name: expectedButtonText });
+
+      await userEvent.click(button);
+
+      expect(previousPage).toHaveBeenCalled();
     });
   });
 });
