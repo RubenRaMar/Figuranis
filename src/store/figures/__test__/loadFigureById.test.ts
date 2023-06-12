91ef50508ffce97d981ea3780659cb92962734a7
RubenRaMar <rubenfr1@hotmail.com>
2023-06-12 02:19:06 +0200

    Test loadFigureById mini reducer and their use cases

---
+++ src/store/figures/__test__/loadFigureById.test.ts
@@ -0,0 +1,21 @@
+import {
+  figureMockFactory,
+  figuresMocksFactory,
+} from "../../../mocks/factory/factories";
+import { figureReducer, loadFigureByIdActionCreator } from "../figureSlice";
+
+describe("Given a loadFigureById mini reducer", () => {
+  describe("When it is invoked with a figure list", () => {
+    test("Then it returns a new list figures with the old figures plus the new figures", () => {
+      const figuresMock = figuresMocksFactory(5);
+      const figureMock = figureMockFactory();
+
+      const loadFigures = figureReducer(
+        { figuresData: figuresMock },
+        loadFigureByIdActionCreator(figureMock)
+      );
+
+      expect(loadFigures).toStrictEqual({ figuresData: [figureMock] });
+    });
+  });
+});
