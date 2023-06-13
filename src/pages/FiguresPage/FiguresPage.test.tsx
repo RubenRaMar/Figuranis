25393fc1ad186b7af74a42529ccc8fa49bd38549
RubenRaMar <rubenfr1@hotmail.com>
2023-06-13 15:11:56 +0200

    Test FiguresPage and their use cases

--- src/pages/FiguresPage/FiguresPage.test.tsx
+++ src/pages/FiguresPage/FiguresPage.test.tsx
@@ -1,14 +1,9 @@
-import { screen } from "@testing-library/react";
+import { screen, waitFor } from "@testing-library/react";
 import FiguresPage from "./FiguresPage";
 import { renderWithProviders } from "../../utils/testUtils";
-import Pagination from "../../components/Pagination/Pagination";
-import { vi } from "vitest";
 import userEvent from "@testing-library/user-event";
 
 describe("Given a FiguresPage component", () => {
-  const nextPage = vi.fn();
-  const previousPage = vi.fn();
-
   describe("When it rendered", () => {
     test("Then it should show a heading with a 'Your favorite figures'", () => {
       const expectedAltText = "Your favorite figures";
@@ -23,14 +18,16 @@ describe("Given a FiguresPage component", () => {
 
   describe("When it is rendered and the user clicks on the 'next' button", () => {
     test("Then it should call the function nextPage", async () => {
-      renderWithProviders(
-        <Pagination
-          nextPage={nextPage}
-          previousPage={previousPage}
-          skip={1}
-          length={10}
-        />
-      );
+      renderWithProviders(<FiguresPage />, {
+        ui: {
+          isLoading: false,
+          modal: { error: false, isModal: false, message: "tot be" },
+          pagination: {
+            skip: 1,
+            limit: 1,
+          },
+        },
+      });
 
       const expectedButtonText = "next";
 
@@ -38,19 +35,22 @@ describe("Given a FiguresPage component", () => {
 
       await userEvent.click(button);
 
-      expect(nextPage).toHaveBeenCalled();
+      waitFor(() => expect(button).toBeDisabled());
     });
   });
+
   describe("When it is rendered and the user clicks on the 'previous' button", () => {
     test("Then it should call the function previousPage", async () => {
-      renderWithProviders(
-        <Pagination
-          nextPage={nextPage}
-          previousPage={previousPage}
-          skip={1}
-          length={10}
-        />
-      );
+      renderWithProviders(<FiguresPage />, {
+        ui: {
+          isLoading: false,
+          modal: { error: false, isModal: false, message: "tot be" },
+          pagination: {
+            skip: 1,
+            limit: 1,
+          },
+        },
+      });
 
       const expectedButtonText = "previous";
 
@@ -58,7 +58,7 @@ describe("Given a FiguresPage component", () => {
 
       await userEvent.click(button);
 
-      expect(previousPage).toHaveBeenCalled();
+      waitFor(() => expect(button).toBeDisabled());
     });
   });
 });
