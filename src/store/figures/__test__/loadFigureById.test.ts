bd0db6b4e4a3c22b158db209c1de507c010daa20
RubenRaMar <rubenfr1@hotmail.com>
2023-12-28 03:14:17 +0100

    Refactor loadFigureById mini reduced test

--- src/store/figures/__test__/loadFigureById.test.ts
+++ src/store/figures/__test__/loadFigureById.test.ts
@@ -1,40 +1,22 @@
 import { figureMockFactory } from "../../../mocks/factory/factories";
-import { FiguresStateStructure } from "../../../types";
+import { currentFigureStateMock } from "../../../mocks/figures/figuresMocks";
+import { FiguresDataStructures, FiguresStateStructure } from "../../../types";
 import { figureReducer, loadFigureByIdActionCreator } from "../figureSlice";
 
 describe("Given a loadFigureById mini reducer", () => {
   describe("When it is invoked with a figure list", () => {
     test("Then it returns a new list figures with the old figures plus the new figures", () => {
-      const figureMock = figureMockFactory();
-
-      const currentState = {
-        figuresData: [],
-        figureData: {
-          id: "",
-          user: "",
-          title: "",
-          character: "",
-          franchise: "",
-          purchased: false,
-          manufacturer: "",
-          material: "",
-          size: 0,
-          weight: 0,
-          price: 0,
-          image: "",
-        },
-        length: 0,
-        filter: false,
-      };
-
-      const action = loadFigureByIdActionCreator(figureMock);
-
-      const loadFigures = figureReducer(currentState, action);
-
+      const figureMock: FiguresDataStructures = figureMockFactory();
       const expectedNewState: FiguresStateStructure = {
-        ...currentState,
+        ...currentFigureStateMock,
         figureData: figureMock,
       };
+
+      const loadFigureAction = loadFigureByIdActionCreator(figureMock);
+      const loadFigures = figureReducer(
+        currentFigureStateMock,
+        loadFigureAction
+      );
 
       expect(loadFigures).toStrictEqual(expectedNewState);
     });
